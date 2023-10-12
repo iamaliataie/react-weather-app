29ac4ada50da825e41c83d5c657d6be59acb5f87
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-12 10:04:56 +0430

    search bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import './App.css';
 import WeatherApp from './Components/WeatherApp/WeatherApp';
 
-import clear from './Components/Assets/clear.png'
-import cloud from './Components/Assets/cloud.png'
-import drizzle from './Components/Assets/drizzle.png'
-import humidity from './Components/Assets/humidity.png'
-import rain from './Components/Assets/rain.png'
-import search from './Components/Assets/search.png'
-import snow from './Components/Assets/snow.png'
-import wind from './Components/Assets/wind.png'
-
-
 function App() {
   return (
     <div className="App">
--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './WeatherApp.css'
 
+import clear from '../Assets/clear.png'
+import cloud from '../Assets/cloud.png'
+import drizzle from '../Assets/drizzle.png'
+import humidity from '../Assets/humidity.png'
+import rain from '../Assets/rain.png'
+import search from '../Assets/search.png'
+import snow from '../Assets/snow.png'
+import wind from '../Assets/wind.png'
+
 const WeatherApp = () => {
   return (
-    <div>WeatherApp</div>
+    <div className='container'>
+        <div className="topbar">
+            <input type="text" placeholder='search city'/>
+            <img src={search} alt="" />
+        </div>
+    </div>
   )
 }
 
