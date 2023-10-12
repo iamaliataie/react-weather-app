44d7e050af48280d59b8fbdf4782d76cc1beb9da
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-12 09:56:38 +0430

    WeatherApp.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import './App.css';
+import WeatherApp from './Components/WeatherApp/WeatherApp';
 
 function App() {
   return (
     <div className="App">
+      <WeatherApp />
     </div>
   );
 }
---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './WeatherApp.css'
+
+const WeatherApp = () => {
+  return (
+    <div>WeatherApp</div>
+  )
+}
+
+export default WeatherApp
