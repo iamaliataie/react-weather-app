1e9e1e50a58543b66583d83437a50161c4eee2ba
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-12 11:04:09 +0430

    weatherapp.jsx updated

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -17,6 +17,27 @@ const WeatherApp = () => {
             <input type="text" placeholder='search city'/>
             <img src={search} alt="" className='search-icon'/>
         </div>
+        <div className="weather-image">
+            <img src={cloud} alt="" />
+        </div>
+        <div className="weather-temp">24c</div>
+        <div className="weather-location">London</div>
+        <div className="data-container">
+            <div className="element">
+                <img src={humidity} alt="" className="icon" />
+                <div className="data">
+                    <div className="humidity-percent">64%</div>
+                    <div className="text">Humidity</div>
+                </div>
+            </div>
+            <div className="element">
+                <img src={wind} alt="" className="icon" />
+                <div className="data">
+                    <div className="humidity-percent">18 km/h</div>
+                    <div className="text">Wind speed</div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
