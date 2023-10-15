906b2d3065638f7021f98831712e6eeba4eef5c2
Ali Ahmad Ataie <iamaliataie@gmail.com>
2023-10-15 09:33:06 +0430

    temp useState

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -14,6 +14,7 @@ const WeatherApp = () => {
     const api_key = 'dc51408f4d141a51ba856de26b7551d6'
 
     const [wIcon, setWeatherIcon] = useState(clear_icon)
+    const [temp, setTemp] = useState('25')
     
     const searchButton = async ()=> {
         const input = document.getElementById('search')
@@ -30,7 +31,7 @@ const WeatherApp = () => {
         const humidity = document.getElementById('humidity')
         const wind = document.getElementById('wind')
         
-        temp.innerHTML = data.main.temp + '&deg;c'
+        setTemp(data.main.temp)
         city.innerHTML = data.name
         humidity.innerHTML = data.main.humidity + '%'
         wind.innerHTML = data.wind.speed + ' ' + 'km/h'
@@ -58,7 +59,7 @@ const WeatherApp = () => {
         <div className="flex justify-center">
             <img src={wIcon} alt="" className='w-40'/>
         </div>
-        <div className="text-8xl text-white" id='temp'>24&deg;c</div>
+        <div className="text-8xl text-white" id='temp'>{temp}&deg;c</div>
         <div className="text-6xl text-white" id='city'>London</div>
         <div className="flex items-center justify-between text-white w-[360px] mx-auto px-4 mt-8">
             <div className="flex items-center justify-start space-x-2">
